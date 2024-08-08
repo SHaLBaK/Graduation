0a90d5c7b650c947c3ad70f2a3d75b62946026c8
Mohammad Shalbak <129525933+SHaLBaK@users.noreply.github.com>
2024-08-08 14:45:03 -0500

    Update Welcome_Page.py

--- Welcome_Page.py
+++ Welcome_Page.py
@@ -15,68 +15,7 @@ st.markdown("""<style> section[data-testid="stSidebar"][aria-expanded="true"]{mi
 #        }
 #    </style>
 #    """, unsafe_allow_html=True)
-############################################
-import streamlit as st
-import os
-from streamlit_navigation_bar import st_navbar
-import pages as pg
-import uuid
 
-def main():
-    # st.set_page_config(page_title="NIRVITA, Your AI Dermatologist Assistant", page_icon="🌟", initial_sidebar_state="collapsed")
-    hide_sidebar_style = """
-        <style>
-            [data-testid="stSidebar"] {
-                display: none;
-            }
-            [data-testid="stSidebarNav"] {
-                display: none;
-            }
-            [data-testid="collapsedControl"] {
-                display: none;
-            }
-        </style>
-    """
-    st.markdown(hide_sidebar_style, unsafe_allow_html=True)
-    pages = ["0_About", "1_Street_Segment_finder ", "2_Crash Map"]
-    styles = {
-        "nav": {
-            "background-color": "#95D2B3",
-        },
-        "span": {
-            "color": "white",
-            "padding": "14px",
-        },
-        "active": {
-            "color": "#365E32",
-            "font-weight": "bold",
-            "padding": "14px",
-        }
-    }
-    options = {
-        "show_menu": False,
-        "show_sidebar": False,
-    }
-    page = st_navbar(
-        pages,
-        styles=styles,
-        options=options
-    )
-
-    functions = {
-        "0_About": pg.show_home,
-        "1_Street_Segment_finder": pg.show_feature,
-        "2_Crash Map": pg.show_about,
-    }
-
-    go_to = functions.get(page)
-    if go_to:
-        go_to()
-
-if __name__ == "__main__":
-    main()
-
-###########################################
 # Title
 st.title("Mobility Plan Database Integration")
 st.subheader("By: Mohammad Shalbak")
